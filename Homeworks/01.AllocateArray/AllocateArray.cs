6a8db9bf0b013c00f5e6fa2cad19e33dbab905e7
emValkovv <em.valkov@gmail.com>
2016-12-11 13:27:58 +0200

    homework for 01.Array
    homework for 01.Array

--- Homeworks/01.AllocateArray/AllocateArray.cs
+++ Homeworks/01.AllocateArray/AllocateArray.cs
@@ -5,13 +5,13 @@
         static void Main()
         {
             int n = int.Parse(Console.ReadLine());
-            int[] newArray = new int[n];
+            int[] array = new int[n];
             Console.WriteLine(0);
             for (int i = 1; i < n; i++)
             {
-                newArray[0] = 0;
-                newArray[i] = newArray[i - 1] + 5;
-                Console.WriteLine(newArray[i]);
+                array[0] = 0;
+                array[i] = array[i - 1] + 5;
+                Console.WriteLine(array[i]);
             }
         }
     }
